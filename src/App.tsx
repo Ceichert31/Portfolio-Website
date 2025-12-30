95ad72558593812dd3346dd688267110e3156278
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-30 15:19:52 -0500

    feat: Added an animation and about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [] = useState(0)
-
   return (
     <>
-      <div>
-      </div>
-      <h1>Christopher Eichert</h1>
-      <p>Hello this is my website</p>
-
-      <strong>Info</strong>
-      <br/>
+     <h1 className='header'>Christopher Eichert</h1>
+     <h2>About me</h2>
+      <p className='paragraph'>Hi! My name is Chris and I am a third-year Game Programming major at Champlain College and a 
+        software developer at the Leahy Center. 
+        I love making games and working in frameworks like .NET to make cool desktop applications! 
+        Working with graphics APIs and optimizing game and software performance is a favorite programming pastime of mine. 
+        Below I have some of my favorite software, graphics, and game projects!</p>
+        
+      <h2>Info</h2>
       <a href="https://github.com/Ceichert31">GitHub</a>
       <br/>
-      <a href="https://pineapple3114.itch.io/">itch.io</a>
+        <a href="https://pineapple3114.itch.io/">itch.io</a>
       <br/>
       <a href="https://www.linkedin.com/in/christophereichert/">LinkedIn</a>
 
-      <br/>
-      <br/>
-      <strong>Projects</strong>
-      <br/>
+      <h2>Projects</h2>
       <a href="https://github.com/Ceichert31/BananaGit">Banana Git</a>
       <br/>
       <a href="https://github.com/Ceichert31/VulkanRenderer">Vulkan Graphics Engine</a>
 
-      <br/>
-      <br/>
-      <strong>Game Projects</strong>
-      <br/>
+      <h2>Games</h2>
       <a href="https://ceichert31.itch.io/modulings">Modulings</a>
       <br/>
       <a href="https://canik.itch.io/dwarvenfortune">Dwarven Fortune</a>
